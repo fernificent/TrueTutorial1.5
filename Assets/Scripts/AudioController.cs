3169d89313a4117cff4da4e94e354d28e8b7b52b
unknown <fvillegas@knights.ucf.edu>
2020-10-03 20:52:07 -0400

    Animation commit

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -9,10 +9,13 @@ public class AudioController : MonoBehaviour
     public AudioClip musicClipTwo;
 
     public AudioSource musicSource;
+
+    Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,25 +25,27 @@ public class AudioController : MonoBehaviour
         {
             musicSource.clip = musicClipOne;
             musicSource.Play();
+            anim.SetInteger("State", 1);
 
         }
 
         if (Input.GetKeyUp(KeyCode.W))
         {
             musicSource.Stop();
-
+            anim.SetInteger("State", 0);
         }
 
         if (Input.GetKeyDown(KeyCode.R))
         {
             musicSource.clip = musicClipTwo;
             musicSource.Play();
+            anim.SetInteger("State", 2);
         }
 
         if (Input.GetKeyUp(KeyCode.R))
         {
             musicSource.Stop();
-
+            anim.SetInteger("State", 0);
         }
 
         if (Input.GetKeyDown(KeyCode.L))
@@ -51,6 +56,11 @@ public class AudioController : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.L))
         {
             musicSource.loop = false;
+        }
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
         }
 
     }
